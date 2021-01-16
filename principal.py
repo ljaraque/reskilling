fb1b0e2b48324d19ec4b05871f5f7cbe06b83645
ljaraque <ljaraque@yahoo.com>
2021-01-16 10:43:10 -0300

    Agregada lista de alumnos

--- principal.py
+++ principal.py
@@ -4,3 +4,9 @@ una_lista = [1,2,3,4,5,6,7]
 
 for elemento in una_lista:
     print("Hola Elemento N°", elemento)
+
+nombres = ["Guido", "Fernando", "Rocío", "Francisca", "Nicolás", "Miguel", "Luis"]
+
+for alumno in nombres:
+    print("Hola Alumno:", alumno)
+
